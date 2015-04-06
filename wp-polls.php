17aa8e6012cb9272024179373cd475f9a9cf54de
Lester <lesterchan@gmail.com>
2015-04-06 11:11:49 +0800

    #40 Proper IP checking

--- wp-polls.php
+++ wp-polls.php
@@ -722,16 +722,16 @@ function display_pollresult($poll_id, $user_voted = '', $display_loading = true)
 ### Function: Get IP Address
 if(!function_exists('get_ipaddress')) {
 	function get_ipaddress() {
-		if (empty($_SERVER["HTTP_X_FORWARDED_FOR"])) {
-			$ip_address = $_SERVER["REMOTE_ADDR"];
-		} else {
-			$ip_address = $_SERVER["HTTP_X_FORWARDED_FOR"];
+		foreach ( array( 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR' ) as $key ) {
+			if ( array_key_exists( $key, $_SERVER ) === true ) {
+				foreach ( explode( ',', $_SERVER[$key] ) as $ip ) {
+					$ip = trim( $ip );
+					if ( filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false ) {
+						return esc_attr( $ip );
+					}
+				}
+			}
 		}
-		if(strpos($ip_address, ',') !== false) {
-			$ip_address = explode(',', $ip_address);
-			$ip_address = $ip_address[0];
-		}
-		return esc_attr($ip_address);
 	}
 }
 
