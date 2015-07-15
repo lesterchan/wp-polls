cf211de1c7bf8d98dc063ea4b16f95e5b3d9b6d2
Lester <lesterchan@gmail.com>
2015-07-15 13:52:15 +0800

    More escaping

--- polls-options.php
+++ polls-options.php
@@ -32,8 +32,8 @@ $id = (isset($_GET['id']) ? intval($_GET['id']) : 0);
 if( isset($_POST['Submit']) && $_POST['Submit'] ) {
 	check_admin_referer('wp-polls_options');
 	$poll_bar_style = strip_tags(trim($_POST['poll_bar_style']));
-	$poll_bar_background = strip_tags(trim($_POST['poll_bar_bg']));
-	$poll_bar_border = strip_tags(trim($_POST['poll_bar_border']));
+	$poll_bar_background = substr( strip_tags( trim( $_POST['poll_bar_bg'] ) ), 0, 6 );
+	$poll_bar_border = substr( strip_tags( trim( $_POST['poll_bar_border'] ) ), 0, 6 );
 	$poll_bar_height = intval($_POST['poll_bar_height']);
 	$poll_bar = array('style' => $poll_bar_style, 'background' => $poll_bar_background, 'border' => $poll_bar_border, 'height' => $poll_bar_height);
 	$poll_ajax_style = array('loading' => intval($_POST['poll_ajax_style_loading']), 'fading' => intval($_POST['poll_ajax_style_fading']));
@@ -43,7 +43,7 @@ if( isset($_POST['Submit']) && $_POST['Submit'] ) {
 	$poll_ans_result_sortorder = strip_tags(trim($_POST['poll_ans_result_sortorder']));
 	$poll_archive_perpage = intval($_POST['poll_archive_perpage']);
 	$poll_archive_displaypoll = intval($_POST['poll_archive_displaypoll']);
-	$poll_archive_url = strip_tags(trim($_POST['poll_archive_url']));
+	$poll_archive_url = esc_url_raw( strip_tags( trim( $_POST['poll_archive_url'] ) );
 	$poll_archive_show = intval($_POST['poll_archive_show']);
 	$poll_currentpoll = intval($_POST['poll_currentpoll']);
 	$poll_close = intval($_POST['poll_close']);
@@ -316,7 +316,7 @@ if( isset($_POST['Submit']) && $_POST['Submit'] ) {
 		</tr>
 		<tr>
 			<th scope="row" valign="top"><?php _e('Poll Archive URL:', 'wp-polls'); ?></th>
-			<td><input type="text" name="poll_archive_url" value="<?php echo esc_attr( get_option( 'poll_archive_url' ) ); ?>" size="50" dir="ltr" /></td>
+			<td><input type="text" name="poll_archive_url" value="<?php echo esc_url( get_option( 'poll_archive_url' ) ); ?>" size="50" dir="ltr" /></td>
 		</tr>
 		<tr>
 			<th scope="row" valign="top"><?php _e('Display Poll Archive Link Below Poll?', 'wp-polls'); ?></th>
@@ -351,9 +351,9 @@ if( isset($_POST['Submit']) && $_POST['Submit'] ) {
 								$poll_question = stripslashes($poll->pollq_question);
 								$poll_id = intval($poll->pollq_id);
 								if($poll_id == intval(get_option('poll_currentpoll'))) {
-									echo "<option value=\"$poll_id\" selected=\"selected\">$poll_question</option>\n";
+									echo '<option value="' . $poll_id . '" selected="selected">' . esc_attr( $poll_question ) . '</option>';
 								} else {
-									echo "<option value=\"$poll_id\">$poll_question</option>\n";
+									echo '<option value="' . $poll_id . '">' . esc_attr( $poll_question ) . '</option>';
 								}
 							}
 						}
